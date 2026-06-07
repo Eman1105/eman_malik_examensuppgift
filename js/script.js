7bf304f3ac661264423b44beec3353c8f18bdb25
Eman1105 <emanmalik9304@gmail.com>
2026-06-07 18:50:45 +0200

    !

--- js/script.js
+++ js/script.js
@@ -34,7 +34,7 @@ document.addEventListener("keydown",function(event){
 
         if (bokstav== "f"){
 
-            document.getElementByld("bild").style.adddisplay = "block";
+            document.getElementById("bild").style.adddisplay = "block";
         }
 
 }); 
