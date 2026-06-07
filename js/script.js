6b24eee1f24c1ce304c064ae47b1e2486aeb1a7b
Eman1105 <emanmalik9304@gmail.com>
2026-06-07 17:10:08 +0200

    ??

--- js/script.js
+++ js/script.js
@@ -1,29 +1,28 @@
 //när tangent trycks ner
-document.addEventListener("keydown",function(event){
+document.addEventListener("keydown", function(event){
 
     //hämtar vilket tangent som trycks ner
-    let bokstav= event.key.toLocaleLowerCase();
+    let bokstav= event.key.toLowerCase();
 
     //leta efter element med smma id
     let knapp = document.getElementById(bokstav);
 
-    //om tangent finns på sidan 
+    //om tangent finns 
     if (knapp){
-
-        //lägg till klassen tryckt
         knapp.classList.add("tryckt");
     }
 
 }); 
 
-document.addEventListener("keyupp",function(event){
+//när tangenten slåpps
+document.addEventListener("keyup",function(event){
 
-    let bokstav = event.key.toLocaleLowerCase();
+    let bokstav = event.key.toLowerCase();
     
     let knapp = document.getElementById(bokstav);
     
     if (knapp) {
-        knapp. classList.remove("tryckt");
+        knapp.classList.remove("tryckt");
     }
 
 }); 
